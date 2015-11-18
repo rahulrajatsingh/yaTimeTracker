1ab6900c1183bd4799e0ce995551cd0ffd2f036b
rahulsinghhfs <rahuls@headfitted.com>
2015-11-18 15:48:34 +0530

    Implementing Authentication and authorization in Website.

--- yaTimeTracker.Website/Controllers/ProjectsController.cs
+++ yaTimeTracker.Website/Controllers/ProjectsController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace yaTimeTracker.Website.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ProjectsController : Controller
     {
         // GET: Dashboard
--- yaTimeTracker.Website/Controllers/ReportsController.cs
+++ yaTimeTracker.Website/Controllers/ReportsController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace yaTimeTracker.Website.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ReportsController : Controller
     {
         // GET: Dashboard
